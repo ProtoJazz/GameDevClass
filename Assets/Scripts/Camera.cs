fbfa7b4dd332596b764513f29dc559211e66346b
Jim MacKenzie <protojazz@gmail.com>
2016-10-06 23:17:49 -0500

    First Class
    Covered : 
    Scene Creation
    Object Creation
    Material Creation
    Physics
    Player Movement
    Camera Movement
    Collisions
    Score Keeping
    GUI

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour {
+    public Transform player;
+    public Vector3 offset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = player.position + offset;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Player : MonoBehaviour {
+    public Rigidbody ourRb;
+    public float moveSpeed;
+    public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        Vector3 moveDirection = new Vector3(moveH, 0, moveV);
+        ourRb.AddForce(moveDirection * moveSpeed);
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Pickup")
+        {
+            Destroy(collision.gameObject);
+            this.score++;
+        }
+    }
+}
